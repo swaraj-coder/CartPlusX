b9a02fbf7f9268c723a1552eef4193d112299673
Swaraj Panja <150075615+swaraj-coder@users.noreply.github.com>
2025-08-02 22:11:07 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -14,7 +14,7 @@ function UserContext({children}) {
 
      const getCurrentUser = async () => {
         try {
-            let result= await axios.get("http://localhost:3000/api/user/getcurrentuser",{withCredentials:true})
+            let result= await axios.get(`${serverUrl}/api/user/getcurrentuser`,{withCredentials:true})
 
             setUserData(result.data)
             console.log(result.data)
